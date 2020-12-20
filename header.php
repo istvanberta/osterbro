5b920a64e54fce0c34f09c1f371e26200b35786f
István Berta <istvan.berta@gmail.com>
2020-12-20 13:44:20 +0100

    Create header.php and footer.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <header>
+      <nav id="primary"></nav>
+    </header>
